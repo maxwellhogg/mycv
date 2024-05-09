3ff7fac2f37f57388dd4f9bfffd2a3695f2e6de4
Maxwell Hogg <44753869+maxwellhogg@users.noreply.github.com>
2024-05-09 07:57:29 +0100

    updated index.php

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+
+$name = 'Maxwell Hogg';
+$mobile = '07407 293 295';
+$email = 'maxy.hogg@googlemail.com';
+$personal_statement = 'I am currently working as a tyre fitter at Nordic Tyres. Although I love the job...';
+$jobs = array(
+    'Nordic Tyres' => array(
+        'Job Title' => 'Tyre Fitter/Warehouse Operative',
+        'Dates' => 'February 2019 - Present',
+        'Responsibilities' => 'Fitting agricultural and forestry tyres, preparing orders for despatch, loading carriers, receiving container deliveries, general housekeeping.',
+    ),
+    'A & G King Timber Ltd.' => array(
+        'Job Title' => 'Machine Operator',
+        'Dates' => 'July 2017 - February 2019',
+        'Responsibilities' => 'Operating various timber processing equipment such as bandsaw, crosscutter, horizontal saw, and chamfering machine, preparing orders for despatch, general housekeeping.',
+    ),
+    'Marshalls PLC (Locharbriggs Quarry)' => array(
+        'Job Title' => 'Stone Polisher/',
+        'Dates' => 'August 2014 - July 2017',
+        'Responsibilities' => ', general housekeeping.',
+    ),
+    'Argos (Church Place)' => array(
+        'Job Title' => 'Stone Polisher/',
+        'Dates' => 'June 2012 - August 2014',
+        'Responsibilities' => ', general housekeeping.',
+    );
+    'A & G King Timber Ltd.' => array(
+        'Job Title' => 'Machine Operator/',
+        'Dates' => 'May 2003 - September 2008',
+        'Responsibilities' => 'Operating various timber processing equipment such as bandsaw, crosscutter, horizontal saw, and chamfering machine, preparing orders for despatch, general housekeeping.',
+    );
+    'Wallaces Express' => array(
+        'Job Title' => 'Machine Operator/',
+        'Dates' => 'July 2002 - May 2003',
+        'Responsibilities' => 'Operating various timber processing equipment such as bandsaw, crosscutter, horizontal saw, and chamfering machine, preparing orders for despatch, general housekeeping.',
+    );
+);
+$education = array(
+	'The University of Glasgow' => array(
+		'Subject' = 'BSc (Hons) Psychology)',
+		'Dates' = '',
+		'Qualification' = '',
+	);
+	'Langside College, Glasgow' => array(
+		'Subject' = 'Access to Humanities',
+		'Dates' = '',
+		'Qualification' = '',
+	);
+	'Dumfries High School' => array(
+		'Subject' = 'BSc (Hons) Psychology)',
+		'Dates' = '',
+		'Qualification' = '',
+	);
+);
+$interests = 'In my spare time, I enjoy...';
+$references = 'I can provide references upon further enquiry.';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Hello, world!</title>
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <meta name="description" content="" />
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+</body>
+</html>
