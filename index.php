d104bc9537e3f9c7f0c8542adb52c59584d91ecb
Maxwell Hogg <44753869+maxwellhogg@users.noreply.github.com>
2024-05-11 06:42:44 +0100

    added to index.php

--- index.php
+++ index.php
@@ -1,107 +1,98 @@
 <?php
-
-$name = 'Maxwell Hogg';
-$mobile = '07407 293 295';
-$email = 'maxy.hogg@googlemail.com';
-$personal_statement = 'I am currently working as a tyre fitter at Nordic Tyres. Although I love the job...';
-$jobs = array(
-    'Nordic Tyres' => array(
-        'Job Title' => 'Tyre Fitter/Warehouse Operative',
-        'Dates' => 'February 2019 - Present',
-        'Responsibilities' => 'Fitting agricultural and forestry tyres, preparing orders for despatch, loading carriers, receiving container deliveries, general housekeeping.',
-    ),
-    'A & G King Timber Ltd.' => array(
-        'Job Title' => 'Machine Operator',
-        'Dates' => 'July 2017 - February 2019',
-        'Responsibilities' => 'Operating various timber processing equipment such as bandsaw, crosscutter, horizontal saw, and chamfering machine, preparing orders for despatch, general housekeeping.',
-    ),
-    'Marshalls PLC (Locharbriggs Quarry)' => array(
-        'Job Title' => 'Stone Polisher/',
-        'Dates' => 'August 2014 - July 2017',
-        'Responsibilities' => ', general housekeeping.',
-    ),
-    'Argos (Church Place)' => array(
-        'Job Title' => 'Stone Polisher/',
-        'Dates' => 'June 2012 - August 2014',
-        'Responsibilities' => ', general housekeeping.',
-    ),
-    'A & G King Timber Ltd.' => array(
-        'Job Title' => 'Machine Operator/',
-        'Dates' => 'May 2003 - September 2008',
-        'Responsibilities' => 'Operating various timber processing equipment such as bandsaw, crosscutter, horizontal saw, and chamfering machine, preparing orders for despatch, general housekeeping.',
-    ),
-    'Wallaces Express' => array(
-        'Job Title' => 'Machine Operator/',
-        'Dates' => 'July 2002 - May 2003',
-        'Responsibilities' => 'Operating various timber processing equipment such as bandsaw, crosscutter, horizontal saw, and chamfering machine, preparing orders for despatch, general housekeeping.',
-    ),
-);
-$education = array(
-	'The University of Glasgow' => array(
-		'Subject' => 'BSc Hons Psychology',
-		'Dates' => '',
-		'Qualification' => '',
-	),
-	'Langside College, Glasgow' => array(
-		'Subject' => 'Access to Humanities',
-		'Dates' => '',
-		'Qualification' => '',
-	),
-	'Dumfries High School' => array(
-		'Subject' => 'BSc (Hons) Psychology)',
-		'Dates' => '',
-		'Qualification' => '',
-	),
-);
-$skills = '';
-$interests = 'In my spare time, I enjoy...';
-$references = 'I can provide references upon further enquiry.';
-$nav_items = [
-	'index.php' => 'CV',
-	'skills.php' => 'Skills',
-	'contact.php' => 'Contact',
-];
-
+	include './includes/header.php';
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  	<meta charset="UTF-8" />
- 	<title><?= $name ?> &bull; <?= $page_title ?></title>
-   <meta name="viewport" content="width=device-width,initial-scale=1" />
-   <meta name="description" content="" />
-   <link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-   <link rel="stylesheet" type="text/css" href="/css/shared.css" />
-   <link rel="stylesheet" type="text/css" href="/css/main.css" />
-</head>
-<body>
-	<?php
-		$page_title = "Home";
-	?>
-	<section class="navbar">
-		<div class="navbar-container">
-			<div class="navbar-logo">
-				<a href="index.php">MAXWELL HOGG - CV</a>
-			</div>
-			<ul class="navbar-list-items">
-				<?php foreach ($nav_items as $page_link => $nav_item): if ($nav_item === 'Contact'): ?>
-					<li class="navbar-list-item navbar-list-item__contact"><a href="<?= $page_link ?>"><?= $nav_item ?></a></li>
-				<?php else: ?>
-					<li class="navbar-list-item"><a href="<?= $page_link ?>"><?= $nav_item ?></a>
-				<?php endif; endforeach; ?>			</ul>
-		</div>
-	</section>
 	<section class="hero">
 		<img src="/images/profile.jpg" alt="My profile image">
 	</section>
-	<section class="personal-statement">
-		<div class="personal-statement-container">
+	<section class="hero-profile">
+		<table class"hero-profile-list">
+			<tr>
+				<td><strong>Name:</strong></td>
+				<td><?= $name ?></td>
+			</tr>
+			<tr>
+				<td><strong>Date of Birth:</strong></td>
+				<td><?= $dob ?></td>
+			</tr>
+			<tr>
+				<td><strong>Tel:</strong></td>
+				<td><?= $tel ?></td>
+			</tr>
+			<tr>
+				<td><strong>Email:</strong></td>
+				<td><a href="mailto:<?= $email ?>"><?= $email ?></td>
+			</tr>
+		</table>
+	</section>
+	<section class="cv-main">
+		<div class="main-container personal-statement">
 			<h1>Personal Statement</h1>
 			<p><?= $personal_statement ?></p>
 		</div>
+		<div class="main-container employment">
+			<h1>Employment</h1>
+			<?php $keys = array_keys($employment);
+			for($i = 0; $i < count($employment); $i++): ?>
+			<div class="info-block">
+			<h3 class="info-heading"><?= $keys[$i] ?></h3>
+			<?php foreach ($employment[$keys[$i]] as $key => $value): ?>
+			<p><strong><?= $key ?></strong>: <?= $value ?></p>
+			<?php endforeach; ?>
+			</div>
+			<?php endfor; ?>
+		</div>
+		<div class="main-container education">
+			<h1>Education</h1>
+			<?php $keys = array_keys($education);
+			for($i = 0; $i < count($education); $i++): ?>
+			<div class="info-block">
+			<h3 class="info-heading"><?= $keys[$i] ?></h3>
+			<?php foreach ($education[$keys[$i]] as $key => $value): ?>
+			<p><strong><?= $key ?></strong>: <?= $value ?></p>
+			<?php endforeach; ?>
+			</div>
+			<?php endfor; ?>
+		</div>
+		<div class="main-container skills">
+			<h1>Skills</h1>
+			<p><?= $skills ?></p>
+		</div>
+		<div class="main-container interests">
+			<h1>Interests</h1>
+			<p><?= $interests ?></p>
+		</div>
+		<div class="main-container references">
+			<h1>References</h1>
+			<p>I can provide references <a href="mailto:<?= $email ?>">upon request</a>.</p>
+		</div>				
 	</section>
+   <footer>
+     <div class="footer-container">
+	 	<div class="social-media">
+	 		<h4>Social Media</h4>
+	 		<ul>
+	 			<li><a href="">Github</a></li>
+	 			<li><a href="">LinkedIn</a></li>
+	 			<li><a href="">Instagram</a></li>
+	 		</ul>
+	 	</div>
+	 	<div class="social-media2">
+	 		<h4>Social Media</h4>
+	 		<ul>
+	 			<li><a href="">Github</a></li>
+	 			<li><a href="">LinkedIn</a></li>
+	 			<li><a href="">Instagram</a></li>
+	 		</ul>
+	 	</div>
+	 	<div class="social-media3">
+	 		<h4>Social Media</h4>
+	 		<ul>
+	 			<li><a href="">Github</a></li>
+	 			<li><a href="">LinkedIn</a></li>
+	 			<li><a href="">Instagram</a></li>
+	 		</ul>
+	 	</div>
+	 </div>
+	 </footer>
 </body>
 </html>
