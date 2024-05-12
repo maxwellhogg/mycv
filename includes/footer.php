e80c2ec47152348e2a24199b2a902f748d10632c
Maxwell Hogg <44753869+maxwellhogg@users.noreply.github.com>
2024-05-12 14:18:53 +0100

    update to includes/footer.php

---
+++ includes/footer.php
@@ -0,0 +1,30 @@
+   <footer>
+     <div class="footer-container">
+	 	<div class="social-media">
+	 		<h4>Social Media</h4>
+	 		<ul>
+	 			<li><a href="">Github</a></li>
+	 			<li><a href="">LinkedIn</a></li>
+	 			<li><a href="">Instagram</a></li>
+	 		</ul>
+	 	</div>
+	 	<div class="social-media2">
+	 		<h4>Social Media</h4>
+	 		<ul>
+	 			<li><a href="">Github</a></li>
+	 			<li><a href="">LinkedIn</a></li>
+	 			<li><a href="">Instagram</a></li>
+	 		</ul>
+	 	</div>
+	 	<div class="social-media3">
+	 		<h4>Social Media</h4>
+	 		<ul>
+	 			<li><a href="">Github</a></li>
+	 			<li><a href="">LinkedIn</a></li>
+	 			<li><a href="">Instagram</a></li>
+	 		</ul>
+	 	</div>
+	 </div>
+	 </footer>
+</body>
+</html>
