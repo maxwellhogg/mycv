9b35103f26ffcbaa675242e9b112e06c3a2c16c1
Maxwell Hogg <44753869+maxwellhogg@users.noreply.github.com>
2024-05-12 14:19:59 +0100

    update to skills.php

--- skills.php
+++ skills.php
@@ -1,27 +1,12 @@
-<!doctype html>
+<?php 
 
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+$page_title = 'Skills';
+include './includes/header.php';
 
-  <title>New HTML5 file</title>
-  <meta name="description" content="A simple new HTML5 file for new projects.">
-  <meta name="author" content="Your name">
-
-  <link rel="icon" href="/favicon.ico">
-  <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-
-  <link rel="stylesheet" href="css/styles.css?v=1.0">
-
-</head>
-
-<body>
-  <!-- your content here... -->
-  <script src="js/scripts.js"></script>
-<?
-echo "hello world";
 ?>
-</body>
-</html>
+
+<?php
+
+include './includes/footer.php';
+
+?>
