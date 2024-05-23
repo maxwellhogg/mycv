97b9390fb3c7928d6fdb7196a67becbb5d73e64c
Maxwell Hogg <maxy.hogg@googlemail.com>
2024-05-23 12:48:25 +0100

    changed profile image and updated common.css

--- includes/header.php
+++ includes/header.php
@@ -76,7 +76,7 @@ $nav_items = [
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="./css/shared.css" />
+	<link rel="stylesheet" type="text/css" href="./css/common.css" />
     <link rel="stylesheet" type="text/css" href="./css/main.css" />
 </head>
 <body>
